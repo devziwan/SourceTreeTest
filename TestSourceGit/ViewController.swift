adddf00384d218322d3c4e146702502ce373575e
devziwan <yoojiwan@icloud.com>
2025-07-30 16:16:56 +0900

    첫번째 주석

--- TestSourceGit/ViewController.swift
+++ TestSourceGit/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(#file, #function, #line, "첫번째 주석")
     }
 
 
