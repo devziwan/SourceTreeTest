480b6b3b5ee1d414783e9a6dac24ffdf12503aac
devziwan <yoojiwan@icloud.com>
2025-07-30 16:12:18 +0900

    프로젝트 생성 커밋 입니다.

---
+++ TestSourceGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSourceGit
+//
+//  Created by 유지완 on 7/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestSourceGitTests/TestSourceGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestSourceGitTests.swift
+//  TestSourceGitTests
+//
+//  Created by 유지완 on 7/30/25.
+//
+
+import Testing
+@testable import TestSourceGit
+
+struct TestSourceGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
